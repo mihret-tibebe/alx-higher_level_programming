ad873ef2c7d746ae03523d396dfdcaf991bd18aa
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-11-30 22:15:53 +0000

    update 3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+if __name__ == '__main__':
+    len = len(sys.argv)
+    result = 0
+    for index in range(len):
+        if index == 0:
+                continue
+        result += int(sys.argv[index])
+    
+    print(result)
