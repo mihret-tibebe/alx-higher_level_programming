b81a3e9a76fd28e7d2ac1666585d03fd997bb56b
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-11-30 22:03:08 +0000

    update 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+if __name__ == '__main__':
+    # print(len(sys.argv))
+    len = len(sys.argv)
+    if len == 1:
+        print('0 arguments.')
+    elif len == 2:
+        print('1 argument:')
+        print(sys.argv[1])
+    else:
+        print("{:d} arguments:".format(len - 1))
+        for index in range(len):
+            if index == 0:
+                continue
+            print("{:d}: {}".format(index, sys.argv[index]))
+
+
