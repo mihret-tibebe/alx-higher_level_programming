9344653b09733c9bb116bd7010200e4c2ae9e276
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-12-05 23:32:14 +0000

    find max value

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if my_list is None or my_list == []:
+        return None
+    max_num = my_list[0]
+    for i in my_list:
+        if max_num < i:
+            max_num = i
+
+    return max_num
