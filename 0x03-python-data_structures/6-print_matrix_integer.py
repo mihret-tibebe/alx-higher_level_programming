a4e4078ddf1e2d434f5b22382f9f8e39b32a9ee4
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-12-05 22:28:29 +0000

    print 3d list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,27 @@
+# #!/usr/bin/python3
+
+# def print_matrix_integer(matrix=[[]]):
+#     for i in matrix:
+#         for j in i:
+#             print("{:d}{}".format(j, " ") if j != i[-1] else "{:d}".format(j), end="")
+#         print("$")
+
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+
+# print_matrix_integer(matrix)
+# print("--")
+# print_matrix_integer()
+
 #!/usr/bin/python3
+
 
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            print("{:d}{}".format(j, " ") if j != i[-1] else "{:d}".format(j), end="")
+            print("{:d}{}".format(j, " ") if j != i[-1] else "{:d}".format(j),
+                  end="")
         print()
