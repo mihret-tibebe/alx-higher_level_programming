17e37395fe781e78caabd21e5b0d56b2a2f8a531
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-12-05 18:36:48 +0000

    add new item to copy of thelist

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,6 +7,14 @@ def new_in_list(my_list, idx, element):
     elif idx > len(my_list) - 1:
         return my_list
     else:
-        new_list = my_list
+        new_list = my_list.copy()
         new_list[idx] = element
         return new_list
+    
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 9
+new_list = new_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)
