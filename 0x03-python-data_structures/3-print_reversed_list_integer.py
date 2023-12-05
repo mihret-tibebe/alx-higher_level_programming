993628c52febae0ba49784cf1cae4f3f389cdab7
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-12-05 18:26:13 +0000

    print reversed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -6,9 +6,3 @@ def print_reversed_list_integer(my_list=[]):
 
     for i in reversed(range(len(my_list))):
         print("{:d}".format(my_list[i]))
-
-
-# print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
-
-my_list = None
-print_reversed_list_integer(my_list)
