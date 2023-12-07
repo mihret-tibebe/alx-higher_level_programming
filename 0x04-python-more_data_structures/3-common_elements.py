fbef8353659a8dbde1f6086df50afc19f48ebeb3
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-12-07 08:33:22 +0000

    search common values

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    return list(filter(lambda x: x in set_2, set_1))
+    new_set = set()
+    for i in set_1:
+        if i in set_2:
+            new_set.add(i)
+    return new_set
