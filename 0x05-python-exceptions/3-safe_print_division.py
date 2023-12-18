e1263b0e43d40881efccb5f6118f118b9b55bb96
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-12-18 23:05:39 +0000

    division by 0

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,10 +2,14 @@
 
 def safe_print_division(a, b):
     result = 0
+    flag = False
     try:
         result = a / b
-        return result
     except ZeroDivisionError:
-        return None
+        flag = True
     finally:
+        if flag is True:
+            print("Inside result: {}".format(None))
+            return None
         print("Inside result: {}".format(result))
+        return result
