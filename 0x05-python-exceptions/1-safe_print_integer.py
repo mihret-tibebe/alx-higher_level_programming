cc635fa083bff8aee41df70fd1be96aaa61caff7
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-12-18 22:35:17 +0000

    print int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        value = int(value)
+        # value = int(value)
         print("{:d}".format(value))
         return True
     except ValueError:
