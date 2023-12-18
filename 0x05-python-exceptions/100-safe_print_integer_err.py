b96dbcf0b363f6b61935f22b77a62e7dcd970d6e
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-12-18 23:53:25 +0000

    print int with msg

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as ex:
+        print("Exception: {}".format(ex))
+
+value = 89
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
