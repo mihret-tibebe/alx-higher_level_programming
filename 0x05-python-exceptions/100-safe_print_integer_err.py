546a76903578b06fcd4fa726112c70ea47dcbc99
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-12-18 23:54:06 +0000

    print int with msg

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,18 +6,3 @@ def safe_print_integer_err(value):
         return True
     except Exception as ex:
         print("Exception: {}".format(ex))
-
-value = 89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
