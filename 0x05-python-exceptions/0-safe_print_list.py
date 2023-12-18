9d26f1a426554156ed5df81dd946c1f5a8ff2ad5
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-12-18 22:17:05 +0000

    updated

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,24 @@
 
 def safe_print_list(my_list=[], x=0):
     new_list = []
+    count = 0
     try:
         for i in range(x):
-            print(i, end='')
-            new_list.append(i)
+            print(my_list[i], end='')
+            new_list.append(my_list[i])
+            count = count + 1
+        print()
     except IndexError:
         print("Out of index")
 
-    return new_list
+    return count
+
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
