2f9fea5d65d525a7eb309cf7cfaf6a2ff64ccb49
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-12-18 22:50:32 +0000

    list int only

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(x):
+            if type(my_list[i]) is int:
+                print("{:d}".format(my_list[i]), end='')
+                count = count + 1
+            continue
+        print()
+        
+    except TypeError:
+        print('')
+    except ValueError:
+        print('')
+    except IndexError as err:
+        raise IndexError('list index out of range')
+
+    return count
