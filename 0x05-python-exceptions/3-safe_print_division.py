e3a81ed89e9b0b0722fd62f3545e996c020aa3b2
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-12-18 23:03:22 +0000

    division by 0

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 
-
 def safe_print_division(a, b):
-    quotient = 0
-    div_by_zero = False
-
+    result = 0
     try:
-        quotient = a / b
+        result = a / b
+        return result
     except ZeroDivisionError:
-        div_by_zero = True
+        return None
     finally:
-        if div_by_zero:
-            print("{}{}".format("Inside result: ", None))
-            return None
-
-        print("{}{}".format("Inside result: ", quotient))
-        return quotient
+        print("Inside result: {}".format(result))
