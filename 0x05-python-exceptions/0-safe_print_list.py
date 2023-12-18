edc8c203811b0d18f56aa1886c4fff46755f462d
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-12-18 22:07:08 +0000

    first commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,11 +3,10 @@
 def safe_print_list(my_list=[], x=0):
     new_list = []
     try:
-        for i in renge(x):
+        for i in range(x):
             print(1, end= '')
             new_list.append(i)
     except IndexError:
         print("Out of index")
 
     return new_list
-    
