c523c339b2206ce4cbc49e2d8e583fd5e7136c23
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-12-18 21:39:14 +0000

    first commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    new_list = []
+    try:
+        for i in renge(x):
+            print(1, end= '')
+            new_list.append(i)
+    except IndexError:
+        print("Out of index")
+
+    return new_list
+    
