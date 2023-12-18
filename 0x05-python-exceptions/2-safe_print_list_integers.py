d8e376bcc8a99373bfccff75900dc8daef594490
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-12-18 22:51:21 +0000

    list int only

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 count = count + 1
             continue
         print()
-        
+
     except TypeError:
         print('')
     except ValueError:
