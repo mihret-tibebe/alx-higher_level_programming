ae76ff30e84141e9ef44097d822df19be10c4567
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-12-19 00:21:02 +0000

    print int with msg

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as ex:
+    except TypeError as ex:
         print("Exception:", ex)
         return False
