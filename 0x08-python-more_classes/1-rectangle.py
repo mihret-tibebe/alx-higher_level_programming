2af3a49ff34b2c155d47082fd76ede5614aeb067
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2024-01-09 11:48:31 +0000

    task

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,9 @@
 class Rectangle():
     """rectangle class
     """
-    def __init__(self, width = 0):
+    def __init__(self, width = 0, height = 0):
         self.__width = width
+        self.__height = height
 
     @property
     def width(self):
@@ -20,3 +21,15 @@ class Rectangle():
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
