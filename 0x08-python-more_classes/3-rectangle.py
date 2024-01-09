0dd970f95cb82d89edf445030b445970f13442b6
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2024-01-09 19:27:18 +0000

    task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -67,17 +67,3 @@ class Rectangle():
                 str_rect += ""
 
         return str_rect
-
-
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-print(str(my_rectangle))
-print(repr(my_rectangle))
-
-print("--")
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle)
-print(repr(my_rectangle))
