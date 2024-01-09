d536ec91126949fbcc2be5cc73621916970f9966
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2024-01-09 11:44:12 +0000

    task

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""rectangle class
+"""
+
+
+class Rectangle():
+    """rectangle class
+    """
+    def __init__(self, width = 0):
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
