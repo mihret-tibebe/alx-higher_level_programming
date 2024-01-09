2c2403082f0d57cd355bdb4155256b393fa3dbd8
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2024-01-09 19:36:27 +0000

    task 4

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""rectangle class
+"""
+
+
+class Rectangle():
+    """rectangle class
+    """
+    def __init__(self, width=0, height=0):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return (2*self.width) + (2*self.height)
+
+    def __str__(self):
+        str_rect = ""
+
+        if self.__height == 0 or self.__width == 0:
+            return ""
+
+        for i in range(self.__height):
+            for j in range(self.__width):
+                str_rect += '#'
+            if i != self.__height - 1:
+                str_rect += '\n'
+            else:
+                str_rect += ""
+
+        return str_rect
+
+    def __repr__(self):
+        return "Rectangle({}{})".format(self.__height, self.__width)
