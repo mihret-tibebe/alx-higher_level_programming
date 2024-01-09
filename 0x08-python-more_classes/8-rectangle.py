17e0eb303fc928c2923bfa7f5e4b848d58815e7e
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2024-01-09 20:21:41 +0000

    task8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -85,11 +85,28 @@ class Rectangle:
         if type(rect_1) is not Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if type(rect_2) is not Rectangle:
-                raise TypeError("rect_2 must be an instance of Rectangle")
+            raise TypeError("rect_2 must be an instance of Rectangle")
 
-        if rect_2.area == rect_1.area:
+        if rect_2.area() == rect_1.area():
             return rect_1
-        elif rect_1.area > rect_2.area:
+        elif rect_1.area() > rect_2.area():
             return rect_1
         else:
             return rect_2
+
+
+my_rectangle_1 = Rectangle(8, 4)
+my_rectangle_2 = Rectangle(2, 3)
+
+if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
+    print("my_rectangle_1 is bigger or equal to my_rectangle_2")
+else:
+    print("my_rectangle_2 is bigger than my_rectangle_1")
+
+
+my_rectangle_2.width = 10
+my_rectangle_2.height = 5
+if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
+    print("my_rectangle_1 is bigger or equal to my_rectangle_2")
+else:
+    print("my_rectangle_2 is bigger than my_rectangle_1")
