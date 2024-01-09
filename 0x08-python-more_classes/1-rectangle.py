7cadba5f3591ff4d26da04d540a362c5633d6f1e
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2024-01-09 11:51:40 +0000

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,14 +6,14 @@
 class Rectangle():
     """rectangle class
     """
-    def __init__(self, width = 0, height = 0):
+    def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if type(value) is not int:
@@ -25,7 +25,7 @@ class Rectangle():
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if type(value) is not int:
