efe950f051ae2f59d0c938cccfabe6e1ecbd61aa
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2024-02-22 01:38:20 +0300

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,4 +2,4 @@
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE first_tablem MODIFY name VARCHAR()256 CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE first_tablem MODIFY name VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_general_ci;
