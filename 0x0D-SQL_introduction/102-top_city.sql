6a2631958f6266abe0bac2dc0f24075266337df5
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2024-02-22 01:43:08 +0300

    Update 102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,4 +1,3 @@
 -- top city
-SELECT city, AVG(value) AS avg_temp FROM (SELECT *  FROM temperatures WHERE
-    month BETWEEN 7 AND 8) AS result GROUP BY result.city ORDER BY avg_temp DESC
-    LIMIT 3 ;
+SELECT city, AVG(value) AS avg_temp FROM (SELECT *  FROM temperatures WHERE 
+    month BETWEEN 7 AND 8) AS result GROUP BY result.city ORDER BY avg_temp DESC LIMIT 3 ;
